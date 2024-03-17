0787ed7d3396543c07804f814d2a601b46fe7272
olavhalleraker <ohallera@gmail.com>
2024-03-17 15:26:58 +0100

    Minor style change

--- actors_same_act.py
+++ actors_same_act.py
@@ -16,12 +16,12 @@ def actors_same_act(actor) -> None:
 
     for act in actor_acts:
         cur.execute(f'''SELECT DISTINCT Teaterstykke.Tittel, Akt.Navn, Ansatt.Navn
-                    FROM Akt JOIN PaaAkt ON Akt.StykkeID = PaaAkt.StykkeID AND Akt.AktNr = PaaAkt.AktNr
-                    JOIN SpillerRolle ON PaaAkt.RolleID = SpillerRolle.RolleID
-                    JOIN Ansatt ON SpillerRolle.AnsattID = Ansatt.AnsattID
-                    JOIN Teaterstykke ON Akt.StykkeID = Teaterstykke.StykkeID
-                    WHERE Akt.StykkeID = {act[0]} AND Akt.AktNr = {act[1]} AND Ansatt.Navn != '{actor}'
-                    ''')
+                FROM Akt JOIN PaaAkt ON Akt.StykkeID = PaaAkt.StykkeID AND Akt.AktNr = PaaAkt.AktNr
+                JOIN SpillerRolle ON PaaAkt.RolleID = SpillerRolle.RolleID
+                JOIN Ansatt ON SpillerRolle.AnsattID = Ansatt.AnsattID
+                JOIN Teaterstykke ON Akt.StykkeID = Teaterstykke.StykkeID
+                WHERE Akt.StykkeID = {act[0]} AND Akt.AktNr = {act[1]} AND Ansatt.Navn != '{actor}'
+                ''')
         for row in cur.fetchall():
             print(f"Stykke: {row[0]} i akt {row[1]} med {row[2]}:")
             
