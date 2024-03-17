eeb3b669bf845818fb14f122a81430cccf898fd8
ComputasMikael <mikael.steenbuch@computas.com>
2024-03-17 12:51:58 +0100

    add test_theatre - rebase main

--- scan-seats-gamle-scene.py
+++ scan-seats-gamle-scene.py
@@ -6,6 +6,7 @@ no_rows_galleri = 3
 no_rows_balkong = 4
 no_rows_parkett = 10
 
+
 def main():
     global sliced_lines
     arguments = sys.argv[1:]
@@ -13,12 +14,12 @@ def main():
     if len(arguments) == 0:
         print("Usage: python scan-seats-gamle-scene.py <file.txt>")
         return
-    
+
     elif len(arguments) > 1:
         print("Too many arguments")
         print("Usage: python scan-seats-gamle-scene.py <file.txt>")
         return
-    
+
     filename = arguments[0]
 
     try:
@@ -26,27 +27,28 @@ def main():
             lines = file.readlines()
 
             for line in lines:
-                line = line[:len(line)-1]
+                line = line[: len(line) - 1]
                 sliced_lines.append(line)
 
     except FileNotFoundError:
         print("File doesn't exist, check file name!")
         return
-    except Exception:
-        print("Error, something went wrong:", Exception)
+    except Exception as e:
+        print("Error, something went wrong:", e)
         return
-    
+
     year, month, day = get_date(sliced_lines)
-    
+
     if (year, month, day) == (None, None, None):
         return
-    
+
     sliced_lines = sliced_lines[1:]
-    
-    while (len(sliced_lines) > 0 and sliced_lines[0] != ""):
+
+    while len(sliced_lines) > 0 and sliced_lines[0] != "":
         find_section_and_reserve_seats()
 
-def get_date(lines):
+
+def get_date(lines: list[str]):
     if "Dato" in lines[0]:
         words = lines[0].split()
 
@@ -69,11 +71,12 @@ def get_date(lines):
             return None, None, None
 
         return year, month, day
-    
+
     else:
         print("Date not found")
         return None, None, None
-    
+
+
 def find_section_and_reserve_seats():
     global sliced_lines
 
@@ -96,19 +99,21 @@ def find_section_and_reserve_seats():
         print("Unknown section", sliced_lines[0])
         return
 
-def get_bought_seats(no_rows):
+
+def get_bought_seats(no_rows: int):
 
     list_of_bought_seats = []
 
     for index_row in range(no_rows):
         seats = list(sliced_lines[index_row])
-        
+
         for index_seat in range(len(seats)):
             if int(seats[index_seat]) == 1:
                 seat_tuple = (index_row + 1, index_seat + 1)
                 list_of_bought_seats.append(seat_tuple)
-    
+
     return list_of_bought_seats
+
 
 if __name__ == "__main__":
     main()
--- scan-seats-hovedscenen.py
+++ scan-seats-hovedscenen.py
@@ -1,8 +1,10 @@
 import sys
 
+
 def main():
     arguments = sys.argv[1:]
     print(arguments)
+
 
 if __name__ == "__main__":
     main()
